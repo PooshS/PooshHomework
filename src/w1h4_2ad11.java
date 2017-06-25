91ac9c749178c5eb431545757e3fd8bdb4e30c41
Poosh <poosh@meta.ua>
2017-06-25 22:28:36 +0300

    some commit

---
+++ src/w1h4_2ad11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by Poosh on 25.06.2017.
+ */
+public class w1h4_2ad11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Укажите ко-во копеек (от 1 до 99): ");
+        int cops = sc.nextInt();
+
+        if(cops > 10 & cops < 20){
+            System.out.println(cops + " копеек");
+        } else {
+            if(cops % 10 == 1)System.out.println(cops + " копейка");
+            else if(cops % 10 >= 2 & cops % 10 <= 4)System.out.println(cops + " копейки");
+            else if(cops % 10 > 4)System.out.println(cops + " копеек");
+            else System.out.println(cops + " копеек");
+        }
+    }
+}
