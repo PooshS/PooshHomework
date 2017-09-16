1bfeb263f42d701043d81794ede9f4ae0748e391
Poosh <poosh@meta.ua>
2017-09-16 19:22:16 +0300

    some commit

---
+++ src/oop/week1/homework/test/Customer.java
@@ -0,0 +1,21 @@
+package oop.week1.homework.test;
+
+/**
+ * Created by Poosh on 14.09.2017.
+ */
+public class Customer {
+
+    String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
