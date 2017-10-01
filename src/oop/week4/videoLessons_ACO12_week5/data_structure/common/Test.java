5ea51013c1441ebe0b90ea2c2edb91249813bc56
PooshS <poosh@meta.ua>
2017-10-01 21:46:58 +0300

    test commit

---
+++ src/oop/week4/videoLessons_ACO12_week5/data_structure/common/Test.java
@@ -0,0 +1,9 @@
+package oop.week4.videoLessons_ACO12_week5.data_structure.common;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+}
